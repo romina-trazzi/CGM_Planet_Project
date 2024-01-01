db8fb4db1029e283018bbc9756e63f0f6f117e03
Romina Trazzi <romina.trazzi@posta.it>
2024-01-02 00:17:53 +0100

    Pulsante reset 3

--- src/components/PlanetButton/PlanetButton.jsx
+++ src/components/PlanetButton/PlanetButton.jsx
@@ -1,7 +1,11 @@
 import './PlanetButton.css';
 
 function PlanetButton({ onReset }) {
-  return <button className ="planet_button" onClick={onReset}>Reset List</button>;
+  return (
+    <button type='reset' className='planet_button' onClick={onReset} autofocus>
+      Reset List
+    </button>
+  );
 }
 
 export default PlanetButton;
